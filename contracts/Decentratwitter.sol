8f9d8541f59063d25f0fc472ae537a808c0a1b7f
Henrychang26 <henrychang26@gmail.com>
2022-10-03 23:18:44 -0700

    final product

--- contracts/Decentratwitter.sol
+++ contracts/Decentratwitter.sol
@@ -43,7 +43,7 @@ event PostTipped(uint256 id, string hash, uint256 tipAmount, address payable aut
         profiles[msg.sender] = _id; //mapping address of the NFT owner to NFT id
     }
 
-    function uploadPost(string memory _postHash) external{
+    function loadPost(string memory _postHash) external{
         //checks that owners has an NFT
         require(balanceOf(msg.sender) > 0, "Must own a decentratwitter to post");
         // Make sure the post hash exists
@@ -53,5 +53,43 @@ event PostTipped(uint256 id, string hash, uint256 tipAmount, address payable aut
         //Add post to the contract
         posts[postCount] = Post(postCount, _postHash, 0, payable(msg.sender));
         emit PostCreated(postCount, _postHash, 0, payable(msg.sender));
+    }
+
+    function tipPostOwner(uint256 _id) external payable {
+        //make sure id is valid
+        require(_id > 0 && _id <= postCount, "invalid post ID");
+        //fetch the post
+        Post memory _post = posts[_id];
+        require(_post.author != msg.sender,"Cannot tip your own post");
+        //pay the author by sending them eth
+        _post.author.transfer(msg.value);
+        //increment the tip amount
+        _post.tipAmount += msg.value; 
+        // L += R => L = L + R
+        //update the image
+        emit PostTipped(_id, _post.hash, _post.tipAmount, _post.author);
+    }
+
+    function getAllPosts() external view returns(Post[] memory _posts){
+        _posts = new Post[](postCount); //(postCount) is the length of memory array
+        //for loop through the array
+        for (uint256 i = 0; i< _posts.length; i++){
+            _posts[i] = posts[i + 1];
+        }
+        // [post2, post3, 0]
+        //  0          1      2
+    }
+    //Fetches all of user's NFTs
+    function getMyNfts() external view returns(uint256[] memory _ids){
+        _ids = new uint256[] (balanceOf(msg.sender));
+        // [balance1, balance2, balance3]
+        uint256 currentIndex; //1 => 2
+        uint256 _tokenCount= tokenCount;
+        for(uint256 i = 0; i <_tokenCount; i++){
+            if(ownerOf(i + 1)== msg.sender){
+                _ids[currentIndex] = i +1;
+                currentIndex++;
+            }
+        }
     }
 }
